45b94bd6ddcadf6b5829ee21869bd65730b13477
Frank Fang <FrankFang1990@gmail.com>
2018-01-28 11:51:12 +0800

    可以调速了

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 !function(){
+  var duration = 50
+  $('.actions').on('click', 'button', function(e){
+    let $button = $(e.currentTarget) // button
+    let speed = $button.attr('data-speed')
+    $button.addClass('active')
+      .siblings('.active').removeClass('active')
+    switch(speed){
+      case 'slow':
+        duration = 100
+        break
+      case 'normal':
+        duration = 50
+        break
+      case 'fast':
+        duration = 10
+        break
+    }
+  })
   function writeCode(prefix, code, fn){
     let container = document.querySelector('#code')
     let styleTag = document.querySelector('#styleTag')
     let n = 0
-    let id = setInterval(()=>{
+    let id
+    id = setTimeout(function run(){
       n+=1
       container.innerHTML = code.substring(0,n)
       styleTag.innerHTML = code.substring(0,n)
       container.scrollTop = container.scrollHeight
-      if(n >= code.length){
-        window.clearInterval(id)
+      if(n < code.length){
+        id = setTimeout(run, duration)
+      }else{
         fn && fn.call()
       }
-    },20)
+    }, duration)
   }
   let code = `
 /*
@@ -175,4 +195,5 @@
  */
 `
   writeCode('',code)
+
 }.call()
